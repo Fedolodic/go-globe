cd3dd98f9b98e5296d64246251903548a4950bf8
David Martinez <david.mtz00@hotmail.com>
2020-05-16 16:50:02 -0500

    Created async request to fetch data from Foursquare API

--- public/main.js
+++ public/main.js
@@ -17,8 +17,23 @@ const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
+const getVenues = async () => {
+    // Value from user's input field
+    const city = $input.val();
+    // Combined text of the entire request URL
+    const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20200516`;
 
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+            console.log(venues);
+            return venues;
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 const getForecast = () => {
